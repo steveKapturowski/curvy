4c69d2ffce20d0b6a317de6d5416d988ee709390
Steven Kapturowski <steven.kapturowski@sendgrid.com>
2015-04-27 01:16:54 -0600

    initial commit for tensor definitions

---
+++ tensors.py
@@ -0,0 +1,23 @@
+import sympy
+
+
+def christoffelSymbols(g):
+	pass
+
+
+def riemannTensor(g):
+	pass
+
+
+def ricciTensor(g):
+	pass
+
+
+def weylTensor(g):
+	pass
+
+
+def ricciScalar(g):
+	pass
+
+
